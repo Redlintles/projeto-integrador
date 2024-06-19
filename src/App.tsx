ddb50ee9c5ec28f9dc0301f66766447147cc2c05
redlintles <treelands54@gmail.com>
2024-06-19 18:03:21 -0300

    Resolvido Conflito em app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
-<<<<<<< Updated upstream
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-=======
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Rodizio from './pages/Rodizio';
-
 
 function App() {
 
@@ -51,7 +18,6 @@ function App() {
       </main>
         <Footer />
      </BrowserRouter>
->>>>>>> Stashed changes
     </>
   )
 }
