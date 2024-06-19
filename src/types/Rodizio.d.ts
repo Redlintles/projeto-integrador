bd315885de7c20eccbfb7402437e51feced812e5
redlintles <treelands54@gmail.com>
2024-06-19 20:44:19 -0300

    Definição de tipos configurada para o projeto

---
+++ src/types/Rodizio.d.ts
@@ -0,0 +1,26 @@
+interface sectorManager {
+  color: string;
+  name: string;
+}
+
+interface rodizioSector {
+  sectorTitle: string;
+  sectorManager: sectorManager;
+}
+
+interface rodizioObj {
+  title: string;
+  cycle: string;
+  desc: string;
+  sectors: rodizioSector[];
+}
+
+interface RodizioMainProps {
+  rodizio: rodizioObj;
+}
+
+type modalType = "" | "add" | "edit";
+interface modalContext {
+  modal: modalType;
+  setModal: React.Dispatch<React.SetStateAction<modalType>>;
+}
