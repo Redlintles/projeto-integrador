5366a1e05c979ff16a28986f174da95e90eb2830
redlintles <treelands54@gmail.com>
2024-06-19 21:02:51 -0300

    Finalizada marcação em RodizioModalEdit

--- src/components/RodizioModal/RodizioModalEdit.tsx
+++ src/components/RodizioModal/RodizioModalEdit.tsx
@@ -1,15 +1,36 @@
-export default function RodizioModalEdit() {
+import { useState } from "react";
+import { BsPencil } from "react-icons/bs";
+
+export default function RodizioModalEdit({ rodizio }: RodizioModalProps) {
+  const [editTitle, setEditTitle] = useState(false);
+
   return (
     <form>
       <form className="modal__form">
-        <label>
-          <span>Nome Do Rodízio</span>
-          <input
-            type="text"
-            placeholder="Digite o nome do rodízio"
-            name="name"
-          />
-        </label>
+        <div>
+          <h2>{rodizio.title}</h2>
+          <button
+            type="button"
+            onClick={() =>
+              setEditTitle((prev) => {
+                return !prev;
+              })
+            }
+          >
+            <BsPencil />
+          </button>
+        </div>
+
+        {editTitle && (
+          <label>
+            <span>Nome Do Rodízio</span>
+            <input
+              type="text"
+              placeholder="Digite o nome do rodízio"
+              name="name"
+            />
+          </label>
+        )}
         <label>
           <span>Descrição</span>
           <textarea
@@ -20,9 +41,9 @@ export default function RodizioModalEdit() {
         <label>
           <span>Tipo de Ciclo</span>
           <select name="cycle">
-            <option value="monthly">Mensal</option>
-            <option value="weekly">Semanal</option>
-            <option value="yearly">Anual</option>
+            <option value="monthly">Mensal(atualização a cada 30 dias)</option>
+            <option value="weekly">Semanal(atualização a cada 7 dias)</option>
+            <option value="yearly">Anual(atualização a cada 365 dias)</option>
           </select>
         </label>
         <label>
