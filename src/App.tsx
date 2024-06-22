df898203839f7de3d8c7f33806f37f1ccc4a83c1
redlintles <treelands54@gmail.com>
2024-06-22 19:33:23 -0300

    Modificado roteamento da página

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Rodizio from "./pages/Rodizio/Rodizio";
 import { ModalContextProvider } from "./context/RodizioContext";
+import Menu from "./pages/Menu/Menu";
 function App() {
   return (
     <>
@@ -12,7 +13,8 @@ function App() {
         <main>
           <ModalContextProvider>
             <Routes>
-              <Route path="/" element={<Rodizio />} />
+              <Route path="/rodizio" element={<Rodizio />} />
+              <Route path="/menu" element={<Menu />} />
             </Routes>
           </ModalContextProvider>
         </main>
